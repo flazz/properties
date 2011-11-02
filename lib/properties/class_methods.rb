f2dc5267a3011092d55b2d6505dfe465da166c50
Francesco Lazzarino <flazzarino@gmail.com>
2011-11-02 15:24:21 -0500

    method definition extracted into class
    Properties::ClassMethods::Definer

--- lib/properties/class_methods.rb
+++ lib/properties/class_methods.rb
@@ -2,26 +2,40 @@ module Properties
 
   module ClassMethods
 
+    class Definer
+      attr_reader :mod
+      attr_reader :name
+
+      def initialize(mod, name)
+        @mod = mod
+        @name = name
+      end
+
+      def define
+        # these are private in ruby, hence the send
+        mod.send :attr_reader, name
+        mod.send :define_method, :"#{name}=", &setter
+        mod.send :define_method, :"#{name}?", &truther
+      end
+
+      def truther
+        name = self.name # ruby wont do instance scope in lambdas
+        proc { send(name.to_sym) ? true : false }
+      end
+
+      def setter
+        name = self.name # ruby wont do instance scope in lambdas
+        proc do |value|
+          instance_variable_set :"@#{name}", value
+          properties.set_changed name
+        end
+      end
+
+    end
+
     def property(name)
       properties << name
-      define_truther name
-      define_setter name
-      define_getter name
-    end
-
-    def define_getter(name)
-      attr_reader name
-    end
-
-    def define_setter(name)
-      define_method(:"#{name}=") do |value|
-        instance_variable_set :"@#{name}", value
-        properties.set_changed name
-      end
-    end
-
-    def define_truther(name)
-      define_method(:"#{name}?") { send(name.to_sym) ? true : false }
+      Definer.new(self, name).define
     end
 
     def properties
